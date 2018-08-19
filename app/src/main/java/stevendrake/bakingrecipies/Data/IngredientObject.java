ad50ceee81f0c410f6f2439d537bc0a9c0a26ae4
SDrake01 <35353948+SDrake01@users.noreply.github.com>
2018-08-18 20:50:03 -0400

    Added java objects for recipes, ingredients, and instructions

---
+++ app/src/main/java/stevendrake/bakingrecipies/Data/IngredientObject.java
@@ -0,0 +1,16 @@
+package stevendrake.bakingrecipies.Data;
+
+public class IngredientObject {
+
+    private static double quantity;
+    private static String measure;
+    private static String ingredient;
+
+    public void setQuantity(double newQuantity){quantity = newQuantity;}
+    public void setMeasure(String newMeasure){measure = newMeasure;}
+    public void setIngredient(String newIngredient){ingredient = newIngredient;}
+
+    public static double getQuantity(){return quantity;}
+    public static String getMeasure() {return measure;}
+    public static String getIngredient() {return ingredient;}
+}
---
+++ app/src/main/java/stevendrake/bakingrecipies/Data/StepsObject.java
@@ -0,0 +1,23 @@
+package stevendrake.bakingrecipies.Data;
+
+public class StepsObject {
+
+    private static int id;
+    private static String shortDesc;
+    private static String description;
+    private static String videoUrl;
+    private static String thumbUrl;
+
+    public void setId(int newId) {id = newId;}
+    public void setShortDesc(String newShort) {shortDesc = newShort;}
+    public void setDescription(String newDesc) {description = newDesc;}
+    public void setVideoUrl(String newVideo) {videoUrl = newVideo;}
+    public void setThumbUrl(String newThumb) {thumbUrl = newThumb;}
+
+    public static int getId(){return id;}
+    public static String getShortDesc() {return shortDesc;}
+    public static String getDescription() {return description;}
+    public static String getVideoUrl() {return videoUrl;}
+    public static String getThumbUrl() {return thumbUrl;}
+}
+
