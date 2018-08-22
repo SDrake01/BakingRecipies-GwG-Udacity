3a9c60a2e683fc9766dd222d3238a7397a2d0c91
SDrake01 <35353948+SDrake01@users.noreply.github.com>
2018-08-22 13:51:50 -0400

    Created a separate class to parse the recipe json data

---
+++ app/src/main/java/stevendrake/bakingrecipes/Utilities/ParseRecipeJson.java
@@ -0,0 +1,35 @@
+package stevendrake.bakingrecipes.Utilities;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import stevendrake.bakingrecipes.Data.RecipeObject;
+import stevendrake.bakingrecipes.UI.MainActivity;
+
+public class ParseRecipeJson {
+
+    private List<RecipeObject> recipeList = new ArrayList<>();
+
+    public void getRecipeData(JSONArray recipeArray){
+        for (int i = 0; i < recipeArray.length(); i++){
+            RecipeObject recipeBuilder = new RecipeObject();
+            try {
+                JSONObject jsonRecipe = recipeArray.getJSONObject(i);
+                recipeBuilder.setId(jsonRecipe.getString("id"));
+                recipeBuilder.setName(jsonRecipe.getString("name"));
+                recipeBuilder.setIngredients(jsonRecipe.getString("ingredients"));
+                recipeBuilder.setSteps(jsonRecipe.getString("steps"));
+                recipeBuilder.setServings(jsonRecipe.getString("servings"));
+                recipeBuilder.setImage(jsonRecipe.getString("image"));
+                recipeList.add(recipeBuilder);
+            }catch (JSONException j){
+                j.printStackTrace();
+            }
+        }
+        MainActivity.recipeGridAdapter.setRecipes(recipeList);
+    }
+}
